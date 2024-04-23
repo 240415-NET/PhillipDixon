b374f316da07361a33be5e0dfc352a05b3e8c7bd
Dixon <philliprdixonaz@gmail.com>
2024-04-23 09:24:54 -0700

    Changes to be committed: 	new file:   GroupProject_22APR2024.csproj 	new file:   Program.cs

---
+++ GroupProject_22APR2024/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace GroupProject_22APR2024;
+
+class Program
+
+{
+    public static void Main()
+    {
+        //variables declared up front
+        bool exitProgram = false;
+        //string[] mealOptions = new string[7];
+        //bool mealsChosen = false;
+        do //do-wile loop to list out menu options
+        {
+            Console.WriteLine("Please Select an Option");
+            Console.WriteLine("1. Run 'Hello World!'");
+            Console.WriteLine("2. Run 'FizzBuzz'");
+            Console.WriteLine("3. Weekly Meal Planner Entry");
+            Console.WriteLine("4. Weekly Meal Planner Update");
+            Console.WriteLine("5. Weekly Meal Planner Selection");
+            Console.WriteLine("6. Exit Program");
+            try //code for option selections. Includes code to catch invalid entries.
+            {
+                int menuSelection = Convert.ToInt32(Console.ReadLine()); //take entry and attempt to convert to an int32
+                switch(menuSelection)
+                {
+                    case 1: 
+                        Console.WriteLine("Thanks for choosing a valid option. Running 'Hello World!' which just writes the text Hello World!");
+                        HelloWorld();
+                        break;
+                    case 2:
+                        Console.WriteLine("Thanks for choosing a valid option."); 
+                        Console.WriteLine("Running 'FizzBuzz' which writes #'s from 1 to 100");
+                        Console.WriteLine("replacing multiples of 3 with 'Fizz', of 5 with 'Buzz', and of both with 'FizzBuzz'");
+                        FizzBuzz();
+                        break;
+                    case 3:
+                        Console.WriteLine("Thanks for choosing the Weekly Meal Planner Entry option!");
+                        Console.WriteLine("Here you can enter 7 meal options");
+                        Console.WriteLine("Please enter meal one");
+                        break;
+                    case 4:
+                        Console.WriteLine("Thanks for choosing the Weekly Meal Planner Update option!");
+                        Console.WriteLine("Here you can update your meal options");
+                        break;
+                     case 5:
+                        Console.WriteLine("Thanks for choosing the Weekly Meal Planner Selection option!");
+                        Console.WriteLine("Here you can randomly select one of your entered meal options");
+                        break;
+                     case 6:
+                        exitProgram = true;
+                        break;         
+                    default://if any invalid option is selected from the menu
+                        Console.WriteLine("That is not a valid option, please try again");
+                        break;
+                }
+            }
+            catch (System.Exception e)
+            {
+                bool didUserRespondCorrectly = false;
+                Console.WriteLine($"{e.Message}");
+                Console.WriteLine("Please enter a valid whole number");
+            do
+            {
+                Console.WriteLine("Do you want to try again? (Y/N)");
+                string userResponse = Console.ReadLine();
+                if (String.IsNullOrEmpty(userResponse) || (userResponse.ToUpper() != "Y" && userResponse.ToUpper() != "N"))
+                {
+                    Console.WriteLine("You must provide a Y or N response");
+                }
+                else if (userResponse.ToUpper() == "N")
+                {
+                    exitProgram = true;
+                    didUserRespondCorrectly = true;
+                }
+                else
+                {
+                    Console.Clear();
+                    didUserRespondCorrectly = true;
+                }
+            }while(didUserRespondCorrectly == false);
+            }
+        } while(exitProgram ==false);
+    }
+
+    public static void HelloWorld()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static void FizzBuzz()
+    {
+         for (int i = 1; i <= 100; i++) //start at 1, loop to 100
+            {
+                if (i % 3 == 0 && i % 5 == 0) //for the #'s evenly divisible by 3 and 5
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0) //for the #'s evenly divisible by 3 only
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0) //for the #'s evenly divisible by 5 only
+                {       
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i); //for all other #'s
+                }
+            }
+    }
+
